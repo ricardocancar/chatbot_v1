bc7558d19c113c70e0635409d357b05c9c48e464
Ricardo Cancar <ricardocancar@gmail.com>
2018-05-15 11:43:50 +0200

    añade variable ejemplo al repositorio

---
+++ variables_ejemplo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+	
+Token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+host =  'xxxxxxxxxxxxxxxxxxxxxxxxx'
+port = xxxxxx
+apiaccess = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+apiDeveloperaccess = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+
+
