00521fa494c55d3279f0c97105982f1496c20a4a
Ricardo Cancar <ricardocancar@gmail.com>
2018-05-23 10:21:16 +0200

    all

--- unitest.py
+++ unitest.py
@@ -13,7 +13,7 @@ class TestDB(unittest.TestCase):
      client = MongoClient('localhost',27017)
      dbUsuario = client.test.test
      return
-  '''
+  
   def test_insertar(self): 
      client = MongoClient('localhost',27017)
      dbUsuario = client.prueba.usuarios
@@ -34,12 +34,13 @@ class TestDB(unittest.TestCase):
      self.assertTrue(idioma =='Cast')
      dbUsuario.delete_one({'_id':555})
  
- # def test_query(self):
- #    self.assertEqual(apiai.query("salario consejal",555,'Cast'),'un Concejal gana 66.495,37€ de retribución anual bruta (sin antigüedad)')
- #    self.assertEqual(apiai.query("salario Sergi",555,'Cast'),'Sergi Campillo Fernández gana 74.342,66€ de retribución anual bruta (sin antigüedad)  por su cargo de Teniente de Alcalde.')
- #    self.assertEqual(apiai.query("impuesto benimaclet",555,'Cast'), 'el barrio Benimaclet pago en total 6.428.288,48 en impuestos el año 2016')
- #    self.assertEqual(apiai.query("ibi benimaclet 2018",555,'Cast'), 'no tenemos los impuesto del 2018 año. de momento solo contamos con los impuesto del año 2016')
- 
+  def test_query(self):
+     self.assertEqual(apiai.query("salario consejal",555,'Cast'),'un Concejal gana 66.495,37€ de retribución anual bruta (sin antigüedad)')
+     self.assertEqual(apiai.query("salario Sergi",555,'Cast'),'Sergi Campillo Fernández gana 74.342,66€ de retribución anual bruta (sin antigüedad)  por su cargo de Teniente de Alcalde.')
+     self.assertEqual(apiai.query("impuesto benimaclet",555,'Cast'), 'el barrio Benimaclet pago en total 6.428.288,48 en impuestos el año 2016')
+     self.assertEqual(apiai.query("ibi benimaclet 2018",555,'Cast'), 'no tenemos los impuesto del 2018 año. de momento solo contamos con los impuesto del año 2016')
+  '''
+
   def test_addComa(self):
      self.assertEqual(addComa('78551.36'),'78.551,36')
      self.assertEqual(addComa(75585),'75.585')
